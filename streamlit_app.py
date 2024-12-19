8b92ce96ddcf41dbf9a6260db1598f41c90df575
COCO <hong.git@yahoo.com>
2024-12-20 02:18:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,13 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Assignment 6")
 
-st.write(
-    "Migrate the RAG pipeline you implemented in the previous assignments to Streamlit. \n"
-    "Implement file upload and chat history. \n"
-    "Allow the user to use its own OpenAI API Key, load it from an st.input inside of st.sidebar \n"
-    "Using st.sidebar put a link to the Github repo with the code of your Streamlit app. \n"
-)
+st.markdown("""
+1. Migrate the RAG pipeline you implemented in the previous assignments to Streamlit.
+2. Implement file upload and chat history.
+3. Allow the user to use its own OpenAI API Key:
+   - Load it from an `st.input` inside of `st.sidebar`.
+4. Using `st.sidebar`, put a link to the Github repo with the code of your Streamlit app.
+---
+""")
+
+
