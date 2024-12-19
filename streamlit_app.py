33b50897398b1bfaa3e8d3686fae7bc754a91799
COCO <hong.git@yahoo.com>
2024-12-20 02:12:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title("💬 Assignment 6")
 
 st.write(
     "Migrate the RAG pipeline you implemented in the previous assignments to Streamlit."
-    "Implement file upload and chat history."
+    "\n Implement file upload and chat history."
     "Allow the user to use its own OpenAI API Key, load it from an st.input inside of st.sidebar"
     "Using st.sidebar put a link to the Github repo with the code of your Streamlit app."
 )
