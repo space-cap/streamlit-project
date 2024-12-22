619c204fbe23b11077efb793f074adeb40c3fe59
COCO <hong.git@yahoo.com>
2024-12-23 02:25:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,14 +20,14 @@ st.title("💬 Assignment 7")
 
 st.markdown("""
 QuizGPT를 구현하되 다음 기능을 추가합니다:
-
+            
 함수 호출을 사용합니다.
-유저가 시험의 난이도를 커스터마이징 할 수 있도록 하고 LLM이 어려운 문제 또는 쉬운 문제를 생성하도록 합니다.
-만점이 아닌 경우 유저가 시험을 다시 치를 수 있도록 허용합니다.
-만점이면 st.ballons를 사용합니다.
-유저가 자체 OpenAI API 키를 사용하도록 허용하고, st.sidebar 내부의 st.input에서 로드합니다.
-st.sidebar를 사용하여 Streamlit app의 코드와 함께 Github 리포지토리에 링크를 넣습니다.
----
+            
+1. 유저가 시험의 난이도를 커스터마이징 할 수 있도록 하고 LLM이 어려운 문제 또는 쉬운 문제를 생성하도록 합니다.
+2. 만점이 아닌 경우 유저가 시험을 다시 치를 수 있도록 허용합니다.
+3. 만점이면 st.ballons를 사용합니다.
+4. 유저가 자체 OpenAI API 키를 사용하도록 허용하고, st.sidebar 내부의 st.input에서 로드합니다.
+5. st.sidebar를 사용하여 Streamlit app의 코드와 함께 Github 리포지토리에 링크를 넣습니다.
 """)
 
 
